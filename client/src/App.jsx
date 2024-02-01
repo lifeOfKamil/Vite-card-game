47c86002c5ee34c323422037c08acbcc9053ba55
Kamil Lepkowski <lepkowskikamil@yahoo.com>
2024-02-01 14:01:35 -0600

    Implement face-up cards and gamble cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,9 +13,12 @@ function App() {
 
 	useEffect(() => {
 		socket.on("connect", () => {
-			document.getElementById("card-back").src = cardBack;
-			document.getElementById("card-back").style.width = "168px";
-			document.getElementById("card-back").style.height = "245px";
+			const cardBackElements = document.getElementsByClassName("card-back");
+			for (let i = 0; i < cardBackElements.length; i++) {
+				cardBackElements[i].src = cardBack;
+				cardBackElements[i].style.width = "168px";
+				cardBackElements[i].style.height = "245px";
+			}
 		});
 
 		socket.on("updateDeck", (updatedDeck) => {
@@ -24,6 +27,10 @@ function App() {
 
 		socket.on("updatePlayerCards", (cards) => {
 			setPlayerCards(cards);
+		});
+
+		socket.on("updateGambleCards", (cards) => {
+			setPlayerGambleCards(cards);
 		});
 
 		socket.on("gameData", ({ gameId, users }) => {
@@ -41,8 +48,12 @@ function App() {
 		});
 
 		return () => {
+			socket.off("connect");
 			socket.off("updateDeck");
 			socket.off("updatePlayerCards");
+			socket.off("updateGambleCards");
+			socket.off("gameData");
+			socket.off("reject");
 		};
 	}, []);
 
@@ -57,31 +68,90 @@ function App() {
 		socket.emit("updatePlayerCards", cards);
 	};
 
+	const startGame = () => {
+		const updatedDeck = [...deck];
+		const cards = [];
+		for (let i = 0; i < 3; i++) {
+			cards.push(updatedDeck.pop());
+		}
+		setDeck(updatedDeck);
+		socket.emit("gambleCards", { updatedDeck, cards });
+		socket.emit("updateGambleCards", cards);
+	};
+
 	return (
 		<>
 			<div className="gameBoard">
 				<div className="cardsInPlay">
 					<div className="userArea">
-						<h4>Player 2</h4>
-						<p>{users.length > 1 ? users[1].id : "Waiting for Player 2"}</p>
+						<div className="userInfo">
+							<h4>Player 2</h4>
+							<p>{users.length > 1 ? users[1].id : "Waiting for Player 2"}</p>
+						</div>
+						<div className="user-cards user-2">
+							<button className="card face-up">
+								{playerGambleCards.length > 0
+									? `${playerGambleCards[0].rank} of ${playerGambleCards[0].suit}`
+									: "No Card"}
+							</button>
+							<button className="card face-up">
+								{playerGambleCards.length > 0
+									? `${playerGambleCards[1].rank} of ${playerGambleCards[1].suit}`
+									: "No Card"}
+							</button>
+							<button className="card face-up">
+								{playerGambleCards.length > 0
+									? `${playerGambleCards[2].rank} of ${playerGambleCards[2].suit}`
+									: "No Card"}
+							</button>
+							<div className="gamble-cards user-2">
+								<img class="card-back" alt="card_back" />
+								<img class="card-back" alt="card_back" />
+								<img class="card-back" alt="card_back" />
+							</div>
+						</div>
 					</div>
 					<div className="dealerArea">
 						<div className="buttonContainer">
 							<button onClick={drawCard} class="playerButton">
 								Draw Card
 							</button>
-							<button onClick={drawCard} class="playerButton">
+							<button onClick={startGame} class="playerButton">
 								Start
 							</button>
 						</div>
-						<img id="card-back" alt="card_back" />
+						<img class="card-back" alt="card_back" />
 						<div className="deck">
 							<Deck cards={deck[deck.length - 1]} />
 						</div>
 					</div>
 					<div className="userArea">
-						<h4>Player 1</h4>
-						<p>{users.length > 0 ? users[0].id : "Waiting for Player 1"}</p>
+						<div className="userInfo">
+							<h4>Player 1</h4>
+							<p>{users.length > 0 ? users[0].id : "Waiting for Player 1"}</p>
+						</div>
+						<div className="user-cards user-1">
+							<button className="card face-up">
+								{playerGambleCards.length > 0
+									? `${playerGambleCards[0].rank} of ${playerGambleCards[0].suit}`
+									: "No Card"}
+							</button>
+							<button className="card face-up">
+								{playerGambleCards.length > 0
+									? `${playerGambleCards[1].rank} of ${playerGambleCards[1].suit}`
+									: "No Card"}
+							</button>
+							<button className="card face-up">
+								{playerGambleCards.length > 0
+									? `${playerGambleCards[2].rank} of ${playerGambleCards[2].suit}`
+									: "No Card"}
+							</button>
+							<div className="gamble-cards user-1">
+								<img class="card-back" alt="card_back" />
+								<img class="card-back" alt="card_back" />
+								<img class="card-back" alt="card_back" />
+							</div>
+						</div>
 					</div>
 				</div>
 				<div className="cardsInHand">
