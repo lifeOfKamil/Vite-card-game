4f940d7f21e97330fb0241fbe8be0ac54a670c6a
Kamil Lepkowski <lepkowskikamil@yahoo.com>
2024-06-18 18:52:06 -0500

    Fix issue with first player having two turns

--- server/models/GameSession.js
+++ server/models/GameSession.js
@@ -13,18 +13,17 @@ class GameSession {
 	}
 
 	setInitialPlayer() {
-		this.currentPlayerId = this.players[0].id;
+		if (this.players.length > 0) {
+			this.currentPlayerId = this.players[0].id;
+		}
 	}
 
 	nextPlayer() {
-		if (this.currentTurnIndex === 0) {
-			this.currentTurnIndex = 1;
-		} else if (this.currentTurnIndex === 1) {
-			this.currentTurnIndex = 0;
+		if (this.players.length > 0) {
+			this.currentTurnIndex = (this.currentTurnIndex + 1) % this.players.length;
+			this.currentPlayerId = this.players[this.currentTurnIndex].id;
+			this.updateGameState();
 		}
-		console.log("current turn index: ", this.currentTurnIndex);
-		this.currentPlayerId = this.players[this.currentTurnIndex].id;
-		this.updateGameState();
 	}
 
 	addPlayer(player) {
@@ -36,25 +35,32 @@ class GameSession {
 	}
 
 	emitInitialGamesState() {
-		this.nextPlayer();
-		this.updateGameState();
+		if (this.players.length > 0) {
+			this.currentTurnIndex = 0; // Ensure the first player starts
+			this.currentPlayerId = this.players[this.currentTurnIndex].id;
+			this.updateGameState();
+		}
 	}
 
 	updateGameState() {
-		const gameState = {
-			currentPlayerId: this.players[this.currentTurnIndex].id,
-			players: this.players.map((player) => ({
-				id: player.id,
-				hand: player.hand,
-				faceUpCards: player.faceUpCards,
-				faceDownCards: player.faceDownCards,
-				isTurn: player.id === this.players[this.currentTurnIndex].id,
-			})),
-		};
-		console.log("Current player's turn: ", this.players[this.currentTurnIndex].id);
-		this.players.forEach((player) => {
-			player.socket.emit("gameState", gameState);
-		});
+		if (this.players.length > 0 && this.currentTurnIndex < this.players.length) {
+			const gameState = {
+				currentPlayerId: this.players[this.currentTurnIndex].id,
+				players: this.players.map((player) => ({
+					id: player.id,
+					hand: player.hand,
+					faceUpCards: player.faceUpCards,
+					faceDownCards: player.faceDownCards,
+					isTurn: player.id === this.players[this.currentTurnIndex].id,
+				})),
+			};
+			console.log("Current player's turn: ", this.players[this.currentTurnIndex].id);
+			this.players.forEach((player) => {
+				player.socket.emit("gameState", gameState);
+			});
+		} else {
+			console.error("Cannot update game state: No players or invalid currentTurnIndex");
+		}
 	}
 
 	removePlayer(playerId) {
@@ -62,13 +68,26 @@ class GameSession {
 		const index = this.players.findIndex((player) => player.id === playerId);
 
 		if (index !== -1) {
-			// Optionally handle the player's cards before removal
 			const [removedPlayer] = this.players.splice(index, 1);
-
-			return removedPlayer; // Return the removed player object for any further handling
+			if (this.players.length === 0) {
+				this.resetGame();
+			} else {
+				this.updateGameState();
+			}
+			return removedPlayer;
 		} else {
 			throw new Error("Player not found");
 		}
+	}
+
+	resetGame() {
+		this.deck = shuffle(generateDeck());
+		this.gameDeck = [];
+		this.currentTurnIndex = 0;
+		this.currentPlayerId = null;
+		this.sevenPlayed = false;
+		this.twoPlayed = false;
+		console.log("Game has been reset");
 	}
 
 	dealInitialCards(player) {
@@ -142,10 +161,19 @@ class GameSession {
 		const playedRank = parseInt(playedCard.rank);
 
 		if (this.sevenPlayed && playedRank > 7 && playedRank !== 10 && playedRank !== 2) {
-			player.hand.push(playedCard); // Put the card back in hand
+			player.hand.push(playedCard);
 			const errorMessage = "You must play a 7 or lower after a 7 has been played";
 			player.socket.emit("error", errorMessage);
 			throw new Error(errorMessage);
+		}
+
+		// 10 can be played at any time and clears the deck
+		if (playedRank === 10) {
+			this.gameDeck = [];
+			this.sevenPlayed = false;
+			this.twoPlayed = false;
+			this.updateGameState();
+			return;
 		}
 
 		if (
@@ -154,7 +182,7 @@ class GameSession {
 			playedRank < parseInt(currentTopCard.rank) &&
 			![2, 10].includes(playedRank)
 		) {
-			player.hand.push(playedCard); // Put the card back in hand
+			player.hand.push(playedCard);
 			player.hand = player.hand.concat(this.gameDeck);
 			this.gameDeck = [];
 			const errorMessage = `You must play a card of equal or higher rank than the current top card (${currentTopCard.rank}). You picked up the game deck.`;
@@ -165,29 +193,23 @@ class GameSession {
 
 		this.gameDeck.push(playedCard);
 
-		if (playedRank === 10) {
-			this.gameDeck = [];
-			this.sevenPlayed = false;
-			this.twoPlayed = false;
-		} else if (playedRank === 7) {
+		if (playedRank === 7) {
 			this.sevenPlayed = true;
 		} else if (playedRank === 2) {
 			this.sevenPlayed = false;
 			this.twoPlayed = true;
-			if (this.deck.length === 0 && player.hand.length === 0) {
-				player.hand = player.hand.concat(player.faceUpCards);
-				player.faceUpCards = [];
-				if (player.hand.length === 0) {
-					player.hand = player.hand.concat(player.faceDownCards);
-					player.faceDownCards = [];
-				}
-				if (player.hand.length > 0) {
-					player.socket.emit("playAnotherCard");
+			// Check if this was the last card in the player's hand
+			if (player.hand.length === 0) {
+				if (player.faceUpCards.length > 0) {
+					player.hand = player.hand.concat(player.faceUpCards);
+					player.faceUpCards = [];
+				} else if (player.faceDownCards.length > 0) {
+					player.socket.emit("playFaceDownCard"); // Prompt player to play a face-down card
+					return;
 				}
 			} else {
 				player.socket.emit("playAnotherCard");
 			}
-			return;
 		} else {
 			this.sevenPlayed = playedCard.rank === "7";
 		}
@@ -196,7 +218,7 @@ class GameSession {
 			this.updateGameState();
 		}
 
-		if (this.deck.length > 0 && player.hand.length < 3) {
+		if (this.deck.length > 0 && player.hand.length < 3 && playedRank !== 2) {
 			this.drawCard(player);
 		} else if (this.deck.length === 0 && player.hand.length === 0) {
 			player.hand = player.hand.concat(player.faceUpCards);
